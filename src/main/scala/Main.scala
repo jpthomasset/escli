786530c1bc540b2bbabf493aef3c3f7c3279d84b
Jean-Pierre Thomasset <jpthomasset@gmail.com>
2016-03-14 23:54:04 +0100

    Add basic CLI handling

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,14 @@
 package escli
 
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
-import akka.stream.ActorMaterializer
-import akka.actor.ActorSystem
+// import akka.http.scaladsl.Http
+// import akka.http.scaladsl.model._
+// import akka.stream.ActorMaterializer
+// import akka.actor.ActorSystem
  
-import scala.concurrent.{Future, Await}
-import scala.concurrent.duration._
+// import scala.concurrent.{Future, Await}
+// import scala.concurrent.duration._
+
+import scala.io.StdIn.readLine
 
 object Main extends SimpleParser {
   def main(args: Array[String]): Unit = {
@@ -17,24 +19,38 @@ object Main extends SimpleParser {
 
     println(s"Querying ${url}")
 
-    implicit val system = ActorSystem()
-    implicit val materializer = ActorMaterializer()
-    implicit val ec = system.dispatcher
+    // implicit val system = ActorSystem()
+    // implicit val materializer = ActorMaterializer()
+    // implicit val ec = system.dispatcher
 
-    val f: Future[HttpResponse] =
-      Http().singleRequest(HttpRequest(uri = url + "/_search"))
-
-
-    f onSuccess {
-      case r =>
-        println("Result " + r)
-        system.terminate()
-    }
-    // println(parse(select, "select * from someindex"))
+    // val f: Future[HttpResponse] =
+    //   Http().singleRequest(HttpRequest(uri = url + "/_search"))
 
 
-    Await.result(f, 5.seconds)
+    // f onSuccess {
+    //   case r =>
+    //     println("Result " + r)
+    //     system.terminate()
+    // }
+    // // println(parse(select, "select * from someindex"))
 
+
+    // Await.result(f, 5.seconds)
+
+
+    println("escli v0.1")
+    Iterator
+      .continually(readLine("escli> "))
+      .takeWhile(_ != null)
+      .foldLeft("")( (acc, line) => {
+        if(line.endsWith(";")) {
+          //println("Got " + acc + line)
+          println(parse(select, acc + " " + line))
+          ""
+        } else {
+          acc + " " + line
+        }
+      })
 
   }
 }
