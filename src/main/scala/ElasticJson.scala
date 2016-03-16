d41a3ff3c9061ca17caaed2b5737b7ab4bc9d642
Jean-Pierre Thomasset <jpthomasset@gmail.com>
2016-03-16 23:10:41 +0100

    Add test on ElasticJson parser

--- src/main/scala/ElasticJson.scala
+++ src/main/scala/ElasticJson.scala
@@ -7,14 +7,14 @@ object ElasticJson {
   case class ShardInfo(total: Int, successful: Int, failed: Int)
   case class Hit(_index: String, _type: String, _id: String, _score: Double, _source: JsValue)
   case class Hits(total: Long, max_score: Double, hits: Array[Hit])
-  case class SearchResponse(took: Int, timed_out: Boolean, _shards: ShardInfo, hits: Hits)
+  case class SearchResponse(took: Long , timed_out: Boolean, _shards: ShardInfo, hits: Hits)
 }
 
 object ElasticJsonProtocol extends DefaultJsonProtocol {
   import ElasticJson._
-  
+
   implicit val shardInfoFormat = jsonFormat3(ShardInfo)
   implicit val hitFormat = jsonFormat5(Hit)
   implicit val hitsFormat = jsonFormat3(Hits)
-  implicit val elasticResponseFormat = jsonFormat4(SearchResponse)
+  implicit val searchResponseFormat = jsonFormat4(SearchResponse)
 }
---
+++ src/test/scala/ElasticJsonSpec.scala
@@ -0,0 +1,58 @@
+import org.scalatest._
+
+import spray.json._
+
+import escli.ElasticJson._
+import escli.ElasticJsonProtocol._
+
+class ElasticJsonSpec extends WordSpec with Matchers {
+
+
+  val jsonShard = """{"total":5,"successful":5,"failed":0}""" 
+
+  val source  = """{
+          "user" : "kimchy",
+          "post_date" : "2009-11-15T14:12:12",
+          "message" : "trying out Elasticsearch"
+        }"""
+
+  val jsonHit =  """{"_index":"twitter","_type":"tweet","_id":"1","_score":1.0,"_source":""" + source + """}"""
+
+  val jsonHits = """{"total":1,"max_score":1.0,"hits":[""" + jsonHit + """]}"""
+
+  val jsonResponse = """{"took":1,"timed_out":false,"_shards":""" + jsonShard + """, "hits": """ + jsonHits + """}"""
+
+  "ElasticJsonProtocol parser" should {
+    "parse a ShardInfo json" in {
+      assertResult(ShardInfo(5, 5, 0)) {
+        jsonShard.parseJson.convertTo[ShardInfo]
+      }
+    }
+
+    "parse a Hit json" in {
+      assertResult(Hit("twitter", "tweet", "1", 1.0, source.parseJson)) {
+        jsonHit.parseJson.convertTo[Hit]
+      }
+    }
+
+    "parse a Hits json" in {
+      val hits = jsonHits.parseJson.convertTo[Hits] 
+      hits should have {
+        'total (1)
+        'max_score (1.0)
+      }
+
+      hits.hits.length should be (1) 
+    }
+
+    "parse a SearchResponse json" in {
+      val response = jsonResponse.parseJson.convertTo[SearchResponse] 
+      response should have {
+        'took (1)
+        'timed_out (false)
+      }
+
+      
+    }
+  }
+}
