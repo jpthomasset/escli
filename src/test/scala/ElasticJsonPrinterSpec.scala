2f9eed998548e546d055f0574d50e175f5ef05e4
Jean-Pierre Thomasset <jpthomasset@gmail.com>
2016-03-25 16:08:14 +0100

    Add some unit tests on ElasticJsonPrinter

--- src/test/scala/ElasticJsonPrinterSpec.scala
+++ src/test/scala/ElasticJsonPrinterSpec.scala
@@ -53,13 +53,52 @@ class ElasticJsonPrinterSpec extends WordSpec with Matchers {
     }
   }
 
+  "columnSize" should {
+    val printer = new ElasticJsonPrinter(_ => {})
+    "return an empty map when hit does not contain any column" in {
+      printer.columnSize(Hit("index", "type", "1", 1, JsArray())) should be (Map.empty[String, Int])
+      }
+    }
+
   "print(hits, queryCols)" should {
     val sb = new StringBuilder()
     val printer = new ElasticJsonPrinter(sb.append(_))
 
+    val jsObject = JsObject(("key1" -> JsString("very long value over 40 chars (bla bla bla bla bla bla bla)"))) 
+    def hit(n:String) = Hit("index", "type", n, 1, jsObject) 
+    val hits = Array(hit("1000"), hit("1001"))
+      
     "output nothing when query is empty" in {
       printer.print(Array.empty[Hit], List.empty[String])
       sb.toString() should be("")
+    }
+
+    "output simple result" in {
+      val expectedResult =
+        "+-------------------------------------------------------------+\n" +
+        "| key1                                                        |\n" +
+        "+-------------------------------------------------------------+\n" +
+        "| very long value over 40 chars (bla bla bla bla bla bla bla) |\n" +
+        "| very long value over 40 chars (bla bla bla bla bla bla bla) |\n" +
+        "+-------------------------------------------------------------+\n"
+
+      sb.clear()
+      printer.print(hits, List.empty[String])
+      sb.toString() should be(expectedResult)
+    }
+
+    "output simple result based on column definition" in {
+      val expectedResult =
+        "+-------------------------------------------------------------+------+\n" +
+        "| key1                                                        | key2 |\n" +
+        "+-------------------------------------------------------------+------+\n" +
+        "| very long value over 40 chars (bla bla bla bla bla bla bla) | null |\n" +
+        "| very long value over 40 chars (bla bla bla bla bla bla bla) | null |\n" +
+        "+-------------------------------------------------------------+------+\n"
+
+      sb.clear()
+      printer.print(hits, "key1" :: "key2" :: Nil)
+      sb.toString() should be(expectedResult)
     }
   }
 
