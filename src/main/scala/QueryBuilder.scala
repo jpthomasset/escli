40774070ace0f5573265675fd04c696397ad8be0
Jean-Pierre Thomasset <jpthomasset@gmail.com>
2016-03-31 07:16:53 +0200

    Building query terms from sql query clause (wip)

--- src/main/scala/QueryBuilder.scala
+++ src/main/scala/QueryBuilder.scala
@@ -7,9 +7,9 @@ object QueryBuilder {
   def build(statement: Statement): Option[Request] = {
     statement match {
       case Select(list, source, where, limit) =>
-        Some(Request(build(source), RequestBody(None, limit.map(_.size), build(list), None)))
+        Some(Request(build(source), RequestBody(None, limit.map(_.size), build(list), build(where))))
       case _ => None
-      }
+    }
   }
 
   def build(selectList: SelectList): Option[Array[String]] =
@@ -20,8 +20,24 @@ object QueryBuilder {
 
   def build(source: Source): String =
     "/" + source.index + (source.documentType match {
-      case Some(t) =>  "/" + t
+      case Some(t) => "/" + t
       case None => ""
     }) + "/_search"
+
+  def build(condition: Option[Condition]): Option[TypedQuery] =
+    condition.map {
+      case TermCondition(field, term) => TypedQuery(TermQuery(field, term))
+      case TermsCondition(field, terms) => TypedQuery(TermsQuery(field, terms))
+      case RangeCondition(field, min, max) => TypedQuery(RangeQuery(field, Some(min), None, Some(max), None))
+      case ComparisonCondition(field, op, value) => op match {
+        case AST.eq => TypedQuery(TermQuery(field, value.toString()))
+        case AST.gt => TypedQuery(RangeQuery(field, None, Some(value), None, None))
+        case AST.lt => TypedQuery(RangeQuery(field, None, None, None, Some(value)))
+        case AST.gte => TypedQuery(RangeQuery(field, Some(value), None, None, None))
+        case AST.lte => TypedQuery(RangeQuery(field, None, None, Some(value), None))
+        }
+
+      case _ => ???
+    }
 
 }
